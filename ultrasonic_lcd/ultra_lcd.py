1e4bbdbb05bbcd2c5d0c80fdf8bee5e2ef6eb417
SunFounder <support@sunfounder.com>
2018-05-03 18:13:19 +0800

    Add files via upload

--- ultrasonic_lcd/ultra_lcd.py
+++ ultrasonic_lcd/ultra_lcd.py
@@ -1,6 +1,7 @@
 import time
 import RPi.GPIO as GPIO
-from lcd import*;
+from lcd import LCD;
+from ultrasonic import Ultrasonic;
 ULTRASONIC_TIMEOUT = 0.5
 
 GPIO.setmode(GPIO.BCM)
@@ -13,45 +14,19 @@ print "Ultrasonic Measurement"
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
 GPIO.setup(GPIO_ECHO, GPIO.IN)
 
-def distance():
-    GPIO.output(GPIO_TRIGGER, False)
-
-    time.sleep(0.5)
-
-    GPIO.output(GPIO_TRIGGER, True)
-    time.sleep(0.01)
-    GPIO.output(GPIO_TRIGGER, False)
-
-    timeout_start = time.time()
-    while GPIO.input(GPIO_ECHO)==0:
-        start = time.time()
-        period = start - timeout_start
-        if period > ULTRASONIC_TIMEOUT:
-            return False
-    #print('start: %s'%start)
-    timeout_start = time.time()
-    stop = 0
-    while GPIO.input(GPIO_ECHO)==1:
-        stop = time.time()
-        period = stop - timeout_start
-        if period > ULTRASONIC_TIMEOUT:
-            return False
-    #print('stop: %s'%stop)
-
-    elapsed = stop-start
-    dis = elapsed * 34300
-    dis = dis / 2
-    return dis
-
 def loop():
     global lcd
     lcd = LCD()
+    us = Ultrasonic(GPIO_TRIGGER, GPIO_ECHO)
     while True:
-        d = distance()
-        lcd.message('distance:\n                ')
-        print('distance:%0.2fcm' % d)
-        #lcd.clear()
-        lcd.message('distance:\n%0.2fcm' % d)
+        distance = us.distance()
+        if distance:
+            lcd.message('distance:\n                ')
+            print('distance:%0.2fcm' % distance)
+            #lcd.clear()
+            lcd.message('distance:\n%0.2fcm' % distance)
+        else:
+            print('Error')
         time.sleep(0.2)
 
 def destroy():
